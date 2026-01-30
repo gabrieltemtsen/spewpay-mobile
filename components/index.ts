5c4da8c6ba5b41128dadbebb7a600a5055579eae
gabrieltemtsen <gabrieltemtsen@gmail.com>
2026-01-30 18:09:29 +0100

    chore: update component exports
    - Add exports for ConfirmationModal, TransactionItem
    - Add exports for SearchBar and FilterChips
    - Fix duplicate export errors

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,10 @@
 export { Button } from './Button';
+export { ConfirmationModal } from './ConfirmationModal';
 export { EmptyState } from './EmptyState';
 export { ErrorBoundary } from './ErrorBoundary';
+export { FilterChips } from './FilterChips';
 export { LoadingScreen } from './LoadingScreen';
+export { SearchBar } from './SearchBar';
 export { ToastProvider, useToast } from './Toast';
+export { TransactionItem } from './TransactionItem';
 

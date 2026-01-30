39ec6abc0c088050bee9b83eae44fa50362ed0ed
gabrieltemtsen <gabrieltemtsen@gmail.com>
2026-01-30 21:42:31 +0100

    chore: export QR code components

--- components/index.ts
+++ components/index.ts
@@ -1,10 +1,12 @@
 export { Button } from './Button';
 export { ConfirmationModal } from './ConfirmationModal';
 export { EmptyState } from './EmptyState';
-export { ErrorBoundary } from './ErrorBoundary';
 export { FilterChips } from './FilterChips';
 export { LoadingScreen } from './LoadingScreen';
-export { SearchBar } from './SearchBar';
+export { QRCodeGenerator } from './QRCodeGenerator';
+export { QRCodeScanner } from './QRCodeScanner';
+export { SearchBar, SearchBar } from './SearchBar';
 export { ToastProvider, useToast } from './Toast';
+export { TransactionDetailModal } from './TransactionDetailModal';
 export { TransactionItem } from './TransactionItem';
 

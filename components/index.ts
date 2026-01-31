7400e88202c568a9715d0b7be1bd791f4f00a2f3
gabrieltemtsen <gabrieltemtsen@gmail.com>
2026-01-31 05:22:46 +0100

    feat: update orgs screen and add org detail page
    - Connect orgs tab to real data via hooks
    - Add CreateOrgModal integration
    - Create org detail screen with balance, members, allocations
    - Export all new components

--- components/index.ts
+++ components/index.ts
@@ -6,8 +6,19 @@ export { LoadingScreen } from './LoadingScreen';
 export { MenuItem, MenuSection } from './Menu';
 export { QRCodeGenerator } from './QRCodeGenerator';
 export { QRCodeScanner } from './QRCodeScanner';
-export { SearchBar, SearchBar } from './SearchBar';
+export { SearchBar } from './SearchBar';
 export { ToastProvider, useToast } from './Toast';
 export { TransactionDetailModal } from './TransactionDetailModal';
 export { TransactionItem } from './TransactionItem';
+
+// Organization & Allocation components
+export { AddRuleModal } from './AddRuleModal';
+export { AllocationCard } from './AllocationCard';
+export { CreateOrgModal } from './CreateOrgModal';
+export { FundAllocationModal } from './FundAllocationModal';
+export { InviteCard } from './InviteCard';
+export { InviteMemberModal } from './InviteMemberModal';
+export { MemberItem } from './MemberItem';
+export { OrgCard } from './OrgCard';
+export { RuleItem } from './RuleItem';
 
